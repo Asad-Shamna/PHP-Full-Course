caccb27ab5127ba7c0b318ed4b7a1135512e3559
Asad <asadshamna02@gmail.com>
2023-08-02 23:25:26 +0530

    variables

---
+++ variables.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = 'PHP';
+
+$age = 20;
+
+echo $age; // 20
+
+$age = 'Twenty';
+
+echo $age; // Twenty
+
+//  ways to declare
+
+$lastName; // Camel-case
+
+/* 
+
+$last_name;
+
+$_12;
+
+$num1;
+
+$_1_num;
+
+*/
+
+?>
